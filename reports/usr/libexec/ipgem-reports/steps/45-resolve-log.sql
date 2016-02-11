ecee8bed85ed7f52a2cee4548fb65203227ae28b
Thomas Equeter <tequeter@users.noreply.github.com>
2016-02-11 16:50:34 +0100

    ignore log entries not DNS-resolved yet
    This fixes a cause of duplicate lines in the todo report (one resolved,
    one unresolved).

--- reports/usr/libexec/ipgem-reports/steps/45-resolve-log.sql
+++ reports/usr/libexec/ipgem-reports/steps/45-resolve-log.sql
@@ -1,5 +1,6 @@
 CREATE TABLE resolved_log AS
     SELECT date, host, desc, src, dns.name AS srcname, dst, proto, dport, null AS tclass, log.rowid AS log_id
     FROM log LEFT JOIN dns ON ( log.src = dns.ip AND "date" >= valid AND "date" < until )
+    WHERE "date" <= ( SELECT MAX( until ) FROM dns )
     GROUP BY log.rowid, "date", host, desc, src, dst, proto, dport, dns.name, tclass
     HAVING valid IS NULL OR valid = MAX( valid );
